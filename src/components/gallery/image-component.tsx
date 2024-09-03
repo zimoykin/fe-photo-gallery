e519e03780400679fb0a8ec258c26bd98c1668fd
Dima <zimoykin@gmail.com>
2024-09-03 23:04:32 +0200

    icon fix

--- src/components/gallery/image-component.tsx
+++ src/components/gallery/image-component.tsx
@@ -13,15 +13,16 @@ const ImageModal: React.FC<Props> = ({ src, onClose, next, prev }: Props) => {
             <div className="modal-content">
                 <img src={src} />
                 <div className='commands'>
-                    <button className="icon-button close-button" onClick={onClose}>
-                        <i className="fas fa-times"></i>
-                    </button>
-                    <button className="icon-button prev-button" onClick={prev}>
+
+                    <i className="fas fa-times" onClick={onClose} />
+                    <i className="fas fa-arrow-left" onClick={prev} />
+                    <i className="fas fa-arrow-right" onClick={next} />
+                    {/* <button className="icon-button prev-button" onClick={prev}>
                         <i className="fas fa-arrow-left"></i>
                     </button>
                     <button className="icon-button next-button" onClick={next}>
                         <i className="fas fa-arrow-right"></i>
-                    </button>
+                    </button> */}
                 </div>
             </div>
         </div>
